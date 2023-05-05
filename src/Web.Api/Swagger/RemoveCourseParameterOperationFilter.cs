2e6732bf8a773302b9265c1c5fe745af129bd7f3
Defalt <ilyaudalov1@mail.ru>
2023-05-05 18:01:16 +0500

    Swagger error fixed

--- src/Web.Api/Swagger/RemoveCourseParameterOperationFilter.cs
+++ src/Web.Api/Swagger/RemoveCourseParameterOperationFilter.cs
@@ -9,12 +9,20 @@ public class RemoveCourseParameterOperationFilter : IOperationFilter
 {
 	public void Apply(OpenApiOperation operation, OperationFilterContext context)
 	{
-		var parametersWithCourseType = context.ApiDescription
+		// При использовании CourseBinder (конвертация courseId в Course),
+		// Swagger разворачивает Course, создавая множество параметров, а также добавляет сам courseId из route, которого на деле в аргументах,
+		// в итоге мы получаем null в ParameterDescriptor
+		// ReSharper disable once ConditionIsAlwaysTrueOrFalse
+		var parametersToDiscard = context.ApiDescription
 			.ParameterDescriptions
-			.Where(desc => desc.ParameterDescriptor.ParameterType == typeof(Course) || desc.ParameterDescriptor.ParameterType == typeof(ICourse))
+			.Where(desc =>
+				desc.ParameterDescriptor is null ||
+				desc.ParameterDescriptor.ParameterType == typeof(Course) ||
+				desc.ParameterDescriptor.ParameterType == typeof(ICourse)
+			)
 			.ToList();
 
-		parametersWithCourseType
+		parametersToDiscard
 			.ForEach(param =>
 			{
 				var toRemove = operation.Parameters.SingleOrDefault(p => p.Name == param.Name);
