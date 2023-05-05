d78d3d2d7bc84f3885203873f9e76225a6695bfa
Defalt <ilyaudalov1@mail.ru>
2023-05-06 01:46:43 +0500

    PolymorphismDocumentFilter: removed unused argument, methods collapsed

--- src/Common.Api.Application/Swagger/PolymorphismDocumentFilter.cs
+++ src/Common.Api.Application/Swagger/PolymorphismDocumentFilter.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.OpenApi.Models;
+﻿using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace Ulearn.Common.Api.Swagger;
@@ -7,11 +6,6 @@ namespace Ulearn.Common.Api.Swagger;
 public class PolymorphismDocumentFilter<T> : IDocumentFilter
 {
 	public void Apply(OpenApiDocument openApiDoc, DocumentFilterContext context)
-	{
-		RegisterSubClasses(context, typeof(T));
-	}
-
-	private static void RegisterSubClasses(DocumentFilterContext context, Type baseType)
 	{
 		var schemaGenerator = context.SchemaGenerator;
 		var derivedTypes = DerivedTypesHelper.GetDerivedTypes(typeof(T));
